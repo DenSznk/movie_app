d947906b2ada65bea64cc7cd0ee26a936a4a6886
DenSznk <densznk@gmail.com>
2023-11-14 22:25:26 +0400

    movie condition

--- script.js
+++ script.js
@@ -10,12 +10,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('last movie?', ''),
-    b = prompt('how much you rate it?', ''),
-    c = prompt('last movie?', ''),
-    d = prompt('how much you rate it?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('last movie?', ''),
+        b = prompt('how much you rate it?', '');
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+};
+
+if (personalMovieDB.count < 10) {
+    console.log("You have been wached a little bit movies");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("You are a classic viewer");
+} else if (personalMovieDB.count >= 30) {
+    console.log("You are a movie buff");
+} else {
+    console.log("Error");
+}
 
 console.log(personalMovieDB);
+
